e2b2511c5a2e8476ed21e83cb88f8968eb5357b7
Sirrobzeroone <44497598+sirrobzeroone@users.noreply.github.com>
2022-06-17 22:00:34 +1000

    Split Upload
    Initial upload for MTG

---
+++ init.lua
@@ -0,0 +1,48 @@
+----------------------------------------------------------
+--        ___ _                         _  _ _____      --
+--       | __| |_____ __ _____ _ _ ___ | \| |_   _|     --
+--       | _|| / _ \ V  V / -_) '_(_-< | .` | | |       --
+--       |_| |_\___/\_/\_/\___|_| /__/ |_|\_| |_|       --
+----------------------------------------------------------
+--         Flowers Node Timer for Minetest Game         --
+----------------------------------------------------------
+
+-- modname and path
+local m_name = minetest.get_current_modname()
+local m_path = minetest.get_modpath(m_name)
+
+-- setup mod global table and registered flowers table
+flowers_nt_minetestgame = {}
+
+
+-- Specific game files to load
+local game_id = Settings(minetest.get_worldpath()..DIR_DELIM..'world.mt'):get('gameid')
+
+-- Check for Ethereal
+local is_ethereal = minetest.get_modpath("ethereal")
+
+	if game_id == "minetest" and not is_ethereal then
+		dofile(m_path.. "/i_mtg_settings.lua" )
+	elseif is_ethereal then
+		minetest.debug("[MOD] flowers_nt_minetestgame - Please download flowers_nt_ethereal for ethereal support")	
+	else
+		minetest.debug("[MOD] flowers_nt_minetestgame - This mod is designed for base Minetest Game, no flower settings have been loaded")	
+	end
+
+-- if Bonemeal mod loaded
+if minetest.get_modpath("bonemeal") ~= nil then
+	dofile(m_path.. "/i_bonemeal_override.lua" )		
+end
+
+
+
+
+
+
+	
+
+
+
+
+
+
